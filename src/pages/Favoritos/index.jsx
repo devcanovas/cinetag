f8df6dae7f1d004b97dedbb9359a6e77d84417f7
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-11 11:28:05 -0300

    Criando mock gato bonifácio

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -2,9 +2,9 @@ import styles from "./Favoritos.module.scss";
 import Banner from "@/components/Banner";
 import Titulo from "@/components/Titulo";
 import Card from "@/components/Card"
+import gatoBonifacio from '@/mocks/gatoBonifacio.json'
 
 export default function Favoritos() {
-
   return (
     <>
       <Banner imagem="favoritos" />
@@ -12,7 +12,7 @@ export default function Favoritos() {
         <h1>Meus favoritos</h1>
       </Titulo>
       <section className={styles.container}>
-        <Card id={'3'} titulo={'O poderoso JavaScript'} capa={'https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img4.png'}/>
+        <Card {...gatoBonifacio} />
       </section>
     </>
   );
